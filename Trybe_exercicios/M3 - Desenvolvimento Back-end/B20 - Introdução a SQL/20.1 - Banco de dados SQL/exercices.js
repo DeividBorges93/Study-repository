4b495c6ec12aefd570924a0cd49e5de074e839ab
DeividBorges93 <deividmborges@gmail.com>
2022-09-01 18:17:29 -0300

    exercicios dia 20.1

---
+++ Trybe_exercicios/M3 - Desenvolvimento Back-end/B20 - Introdução a SQL/20.1 - Banco de dados SQL/exercices.js
@@ -0,0 +1,20 @@
+Exercício1 = 'Descubra como fazer uma pesquisa em qualquer tabela sem usar uma linha de código.'
+Resposta = 'Basta clicar com botão direito na tabela e clicar em "select rows - limit 200" O limite vai depender da configuração do workbench.'
+
+Exercício2 = 'Descubra como é possível criar uma tabela sem usar código SQL.'
+Resposta = 'Basta clicar em create table usando o botão direito.'
+
+Exercício3 = 'Feito isso, crie a seguinte tabela com as seguintes restrições:'
+Resposta = 'Basta usar o GUI do Mysql Workbench para fazer isso.'
+
+Exercício4 = 'Analise a tabela city e encontre a tabela à qual a coluna country_id faz referência.'
+Resposta = 'Ela faz referência à tabela country.'
+
+Exercício5 = 'Após resolver o exercícioa =terior, responda: qual tipo de relacionamento a tabela city faz com a tabela country?'
+Resposta = 'N:1'
+
+Exercício6 = 'Qual tipo de relacionamento a tabela country faz com a tabela city?'
+Resposta = '1:N'
+
+Exercício7 = 'Abra tabela por tabela do banco sakila e encontre no mínimo 3 exemplos de um relacionamentos 1:N ou N:1.'
+Resposta = '1 - store -> staff, 2 - country -> city, 3 - film -> language'
