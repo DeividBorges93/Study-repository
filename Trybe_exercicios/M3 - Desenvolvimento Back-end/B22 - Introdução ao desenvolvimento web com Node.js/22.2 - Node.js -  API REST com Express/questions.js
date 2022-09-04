bab99bf13604366be5a093ba910c2cf387c94743
DeividBorges93 <deividmborges@gmail.com>
2022-09-04 15:12:08 -0300

    respostas Pergunta 1 e Pergunta 2

---
+++ Trybe_exercicios/M3 - Desenvolvimento Back-end/B22 - Introdução ao desenvolvimento web com Node.js/22.2 - Node.js -  API REST com Express/questions.js
@@ -0,0 +1,6 @@
+P1 = 'Para que é usado o código de status 400 e 422?';
+R1a = '400 Bad Request: Essa resposta significa que o servidor não entendeu a requisição, pois está com uma sintaxe inválida.';
+R1b = '422 Unprocessable Entity: A requisição está bem formada mas inabilitada para ser seguida devido aos erros semânticos.'
+
+P2 = 'Para que é usado o código de status 401?';
+R2 = '401 Unauthorized: Embora o padrão HTTP especifique "unauthorized", semanticamente, essa resposta significa "unauthenticated". Ou seja, o cliente deve se autenticar para obter a resposta solicitada.'
