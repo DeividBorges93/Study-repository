5d2d5b96e1a5a0b1ab2f7532a11cf0b2a4609218
DeividBorges93 <deividmborges@gmail.com>
2022-09-04 14:58:18 -0300

    feat: :sparkles: exercicio5
    implementa lógica para aparecer em qual categoria cada IMC corresponde

--- Trybe_exercicios/M3 - Desenvolvimento Back-end/B22 - Introdução ao desenvolvimento web com Node.js/22.1 - Intro Node - Node.js - Runtime Assíncrono/bmi.js
+++ Trybe_exercicios/M3 - Desenvolvimento Back-end/B22 - Introdução ao desenvolvimento web com Node.js/22.1 - Intro Node - Node.js - Runtime Assíncrono/bmi.js
@@ -8,7 +8,47 @@ function handleBMI(weight, height) {
 
   const bmi = weight / heightSquared;
   
+
   return bmi;
+}
+
+const BMI_MAX_AND_MIN = {
+  'Underweight': {
+    minBMI: 0,
+    maxBMI: 18.4,
+  },
+  'Normal Weight': {
+    minBMI: 18.5,
+    maxBMI: 24.9,
+  },
+  'Overweight': {
+    minBMI: 25,
+    maxBMI: 29.9,
+  },
+  'Obese Class I': {
+    minBMI: 30.0,
+    maxBMI: 34.9,
+  },
+  'Obese Class II': {
+    minBMI: 35,
+    maxBMI: 39.9,
+  },
+  'Obese Class III': {
+    minBMI: 40,
+    maxBMI: 100, // Um valor default máximo qualquer, impossível de alcançar no imc.
+  },
+};
+
+function handleBMIResult(imc) {
+  const situacoes = Object.keys(BMI_MAX_AND_MIN);
+
+  const resultFind = situacoes.find((situacao) => {
+    const { maxBMI, minBMI } = BMI_MAX_AND_MIN[situacao];
+
+    return imc >= minBMI && imc <= maxBMI;
+  });
+
+  return resultFind;
 }
 
 function main() {
@@ -16,8 +56,9 @@ function main() {
   const heightInCm = readLine.questionInt('What\'s your height? (cm)')
 
   const bmi = handleBMI(weightInKg, heightInCm);
+  const bmiResult = handleBMIResult(bmi);
 
-  console.log(`BMI: ${bmi.toFixed(2)}`);
+  console.log(bmiResult);
 }
 
 main();
